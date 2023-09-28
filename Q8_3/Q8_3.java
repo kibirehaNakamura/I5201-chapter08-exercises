f6e186daa4d131a77c8580e9e5a55ad9fbcc8aa3
Nakamura <kisy23013@kibireha.ac.jp>
2023-09-28 10:16:41 +0900

    Q8_3.javaを追加

---
+++ Q8_3/Q8_3.java
@@ -0,0 +1,12 @@
+public class Q8_3 {
+	public static void main(String[] args) {
+		int total;
+		int a1;
+		int a2;
+		
+		a1 = Integer.parseInt(args[0]);
+		a2 = Integer.parseInt(args[1]);
+		total = a1 + a2;
+		System.out.println(a1 + " + " + a2 + " = " + total);
+	}
+}
